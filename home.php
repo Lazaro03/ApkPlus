7cdec3907624e0ffc3a91858f0cddd118074643c
Lazaro03 <95556479+Lazaro03@users.noreply.github.com>
2023-08-12 16:58:33 -0400

    Create home.php

---
+++ home.php
@@ -0,0 +1,245 @@
+{
+    "status": "success",
+    "message": "",
+    "recents": [
+        {
+            "serial": 83,
+            "title": "7zip",
+            "img": "https://revmedest.sld.cu/public/site/images/lazaro03/index.jpg",
+            "id": "https://soft.uclv.edu.cu/7zip/7z2101.exe",
+            "likes": "103",
+            "comments": "15"
+        },
+        {
+            "serial": 83,
+            "title": "AVG PC TuneUp",
+            "img": "https://revmedest.sld.cu/public/site/images/lazaro03/xcxcxz.jpg",
+            "id": "https://ftp.unah.edu.cu/Herramientas/Mantenimiento/AVG%20PC%20Tuneup/AVG%20PC%20TuneUp.2015.v15.0.1001.238-TE.rar",
+            "likes": "103",
+            "comments": "15"
+        },
+        {
+            "serial": 83,
+            "title": "Visual.Studio.Code",
+            "img": "https://revmedest.sld.cu/public/site/images/lazaro03/%C3%ADndicez1.jpg",
+            "id": "https://soft.uclv.edu.cu/Microsoft/Visual.Studio.Code/VSCodeUserSetup-x64-1.44.2.exe",
+            "likes": "103",
+            "comments": "15"
+        },
+        {
+            "serial": 83,
+            "title": "KMPlayer",
+            "img": "https://revmedest.sld.cu/public/site/images/lazaro03/%C3%ADndice.png",
+            "id": "https://soft.uclv.edu.cu/KMPlayer/KMPlayer_4.2.2.26.exe",
+            "likes": "103",
+            "comments": "15"
+        },
+        {
+            "serial": 83,
+            "title": "GOM.Player",
+            "img": "https://revmedest.sld.cu/public/site/images/lazaro03/index.png",
+            "id": "https://soft.uclv.edu.cu/GOM.Player/GOM%20Player%20Plus%202.3.30.5289/GOM%20Player%20Plus%202.3.30.5289.rar",
+            "likes": "103",
+            "comments": "15"
+        },
+        {
+            "serial": 83,
+            "title": "Supercopier 04",
+            "img": "https://revmedest.sld.cu/public/site/images/lazaro03/1.png",
+            "id": "https://dspace.uclv.edu.cu/bitstream/item/18257/Supercopier%2004.exe",
+            "likes": "103",
+            "comments": "15"
+        },
+        {
+            "serial": 83,
+            "title": "Win11_22H2_Spanish_x64",
+            "img": "https://revmedest.sld.cu/public/site/images/lazaro03/descarga1.png",
+            "id": "http://ftp.uo.edu.cu/Windows/Sistemas_Operativos/Windows_11/Win11_22H2_Spanish_x64.iso",
+            "likes": "103",
+            "comments": "15"
+        },
+        {
+            "serial": 80,
+            "title": "Simplewall",
+            "img": "https://revinformatica.sld.cu/public/site/images/lazaro03/image_2023-02-11_15-09-471.png",
+            "id": "https://ftp.unah.edu.cu/Internet%20%26%20Correo/Firewall/Simplewall/simplewall-3.2.4-setup.exe",
+            "likes": "103",
+            "comments": "15"
+        }
+    ],
+    "upper": [
+        {
+            "serial": 21,
+            "title": "AIMP",
+            "img": "https://revmedest.sld.cu/public/site/images/lazaro03/indexs.jpg",
+            "id": "https://soft.uclv.edu.cu/AIMP/aimp_5.01.2358.exe",
+            "likes": "35",
+            "comments": "20"
+        },
+        {
+            "serial": 21,
+            "title": "Picasa",
+            "img": "https://revmedest.sld.cu/public/site/images/lazaro03/%C3%ADndicecc.png",
+            "id": "https://ftp.unah.edu.cu/Media/Picasa/Picasa%203.90%20Build%20137.81.exe",
+            "likes": "35",
+            "comments": "20"
+        },
+        {
+            "serial": 21,
+            "title": "Nox_Setup_v7.0.1.5",
+            "img": "https://revmedest.sld.cu/public/site/images/lazaro03/images.jpg",
+            "id": "https://ftp.unah.edu.cu/Herramientas/Emulador%20de%20android/nox_setup_v7.0.1.5_full_intl.exe",
+            "likes": "35",
+            "comments": "20"
+        },
+        {
+            "serial": 21,
+            "title": "Notepad++",
+            "img": "https://revmedest.sld.cu/public/site/images/lazaro03/sassa.jpg",
+            "id": "https://soft.uclv.edu.cu/Notepad++/npp.7.9.5.Installer.x64.exe",
+            "likes": "35",
+            "comments": "20"
+        },
+        {
+            "serial": 21,
+            "title": "MemuPlay",
+            "img": "https://revmedest.sld.cu/public/site/images/lazaro03/%C3%ADndice.jpg",
+            "id": "https://soft.uclv.edu.cu/MemuPlay/Memu-Setup.exe",
+            "likes": "35",
+            "comments": "20"
+        },
+        {
+            "serial": 21,
+            "title": "KMSAuto Net",
+            "img": "https://revmedest.sld.cu/public/site/images/lazaro03/5.png",
+            "id": "https://dspace.uclv.edu.cu/bitstream/item/18257/KMSAuto%20Net.exe",
+            "likes": "35",
+            "comments": "20"
+        },
+        {
+            "serial": 21,
+            "title": "Bdcamsetup",
+            "img": "https://smi.cujae.edu.cu/file-upload/wWLT7Xro5Zi6fKQSs/1.ico?rc_uid=qyxzWSpbC6mHCQv4u&rc_token=dToQQHt7RUFw_ffp7s5W8L7eufrWXOwdZNKu4Y2Dpmc",
+            "id": "https://dspace.uclv.edu.cu/bitstream/item/18257/bdcamsetup.exe",
+            "likes": "35",
+            "comments": "20"
+        },
+        {
+            "serial": 21,
+            "title": "Net Limiter",
+            "img": "https://revinformatica.sld.cu/public/site/images/lazaro03/image_2023-02-11_15-22-30.png",
+            "id": "https://ftp.unah.edu.cu/Internet%20%26%20Correo/Redes/Net%20Limiter/NetLimiter.3.Pro.v3.0.0.10.rar",
+            "likes": "35",
+            "comments": "20"
+        }
+    ],
+    "trending": [
+        {
+            "serial": 39,
+            "title": "Zapia",
+            "img": "https://revmedest.sld.cu/public/site/images/lazaro03/icon1.png",
+            "id": "https://dspace.uclv.edu.cu/bitstream/item/18257/Zapya.apk",
+            "likes": "35",
+            "comments": "20"
+        },
+        {
+            "serial": 39,
+            "title": "ZArchiver Pro_0.9.5",
+            "img": "https://revmedest.sld.cu/public/site/images/lazaro03/ic_launcher_round.png",
+            "id": "https://dspace.uclv.edu.cu/bitstream/item/18257/ZArchiver%20Pro_0.9.5.apk",
+            "likes": "35",
+            "comments": "20"
+        },
+        {
+            "serial": 39,
+            "title": "ZTK",
+            "img": "https://revmedest.sld.cu/public/site/images/lazaro03/app_icon.png",
+            "id": "https://smi.cujae.edu.cu/file-upload/5T43nNBkb3FtTTk83/ZTK.apk?rc_uid=qyxzWSpbC6mHCQv4u&rc_token=dToQQHt7RUFw_ffp7s5W8L7eufrWXOwdZNKu4Y2Dpmc",
+            "likes": "35",
+            "comments": "20"
+        },
+        {
+            "serial": 40,
+            "title": "PlayPlus(Beta)",
+            "img": "https://smi.cujae.edu.cu/file-upload/6uwvk3kn2aankAmdw/logo.jpg?rc_uid=qyxzWSpbC6mHCQv4u&rc_token=dToQQHt7RUFw_ffp7s5W8L7eufrWXOwdZNKu4Y2Dpmc",
+            "id": "https://smi.cujae.edu.cu/file-upload/e9GeKpZ6QsETpYTQQ/PlayPlus(Beta).apk?rc_uid=qyxzWSpbC6mHCQv4u&rc_token=dToQQHt7RUFw_ffp7s5W8L7eufrWXOwdZNKu4Y2Dpmc",
+            "likes": "103",
+            "comments": "59"
+        },
+        {
+            "serial": 41,
+            "title": "Cortafuegos_sin_Root",
+            "img": "https://revmedest.sld.cu/public/site/images/lazaro03/launcher.png",
+            "id": "https://dspace.uclv.edu.cu/bitstream/item/18257/Cortafuegos_sin_Root.apk",
+            "likes": "103",
+            "comments": "59"
+        },
+        {
+            "serial": 41,
+            "title": "AddBordas_",
+            "img": "https://revmedest.sld.cu/public/site/images/lazaro03/app_icon2.png",
+            "id": "https://dspace.uclv.edu.cu/bitstream/item/18257/AddBordas_.apk",
+            "likes": "103",
+            "comments": "59"
+        },
+        {
+            "serial": 41,
+            "title": "Datally",
+            "img": "https://revmedest.sld.cu/public/site/images/lazaro03/ic_datally.png",
+            "id": "https://dspace.uclv.edu.cu/bitstream/item/18257/Datally.apk",
+            "likes": "103",
+            "comments": "59"
+        }
+    ],
+    "popular": [
+        {
+            "serial": 3,
+            "title": "Truck Simulator_Europe 2",
+            "img": "https://revmedest.sld.cu/public/site/images/lazaro03/app_icon4.png",
+            "id": "https://dspace.uclv.edu.cu/bitstream/item/18257/Truck%20Simulator%20_%20Europe%202.apk",
+            "likes": "30",
+            "comments": "00"
+        },
+        {
+            "serial": 3,
+            "title": "Saw Youtubers Game",
+            "img": "https://revmedest.sld.cu/public/site/images/lazaro03/icon.png",
+            "id": "https://dspace.uclv.edu.cu/bitstream/item/18257/saw%20youtubers%20game.apk",
+            "likes": "30",
+            "comments": "00"
+        },
+        {
+            "serial": 2,
+            "title": "Loop_Taxi_1.46",
+            "img": "https://revmedest.sld.cu/public/site/images/lazaro03/app_icon3.png",
+            "id": "https://dspace.uclv.edu.cu/bitstream/item/18257/Loop%20Taxi.apk",
+            "likes": "30",
+            "comments": "00"
+        },
+        {
+            "serial": 4,
+            "title": "Frontiergunners_0.10.6",
+            "img": "https://revinformatica.sld.cu/public/site/images/lazaro03/image_2023-02-11_15-39-39.png",
+            "id": "http://ftp.uo.edu.cu/Android/Linux/Android/Juegos/Games/Shooter/frontiergunners_0.10.6.apk",
+            "likes": "50",
+            "comments": "00"
+        },
+        {
+            "serial": 5,
+            "title": "Sentinel3home",
+            "img": "https://revinformatica.sld.cu/public/site/images/lazaro03/image_2023-02-11_15-45-271.png",
+            "id": "http://ftp.uo.edu.cu/Android/Linux/Android/Juegos/Games/Shooter/Sentinel3home%28www.m4rtyn3zk.com.ar%29.apk",
+            "likes": "50",
+            "comments": "00"
+        },
+        {
+            "serial": 1,
+            "title": "Heavygunner",
+            "img": "https://revinformatica.sld.cu/public/site/images/lazaro03/image_2023-02-11_15-45-01.png",
+            "id": "http://ftp.uo.edu.cu/Android/Linux/Android/Juegos/Games/Shooter/heavygunner_1.0.8.apk",
+            "likes": "30",
+            "comments": "00"
+        }
+        
+    ]
+}
